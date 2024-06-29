1941426acb5a89ea6331eb8fe89c1dcb7163f9da
shoknt <153351426+shoknt@users.noreply.github.com>
2024-06-29 12:03:31 +0700

    Update discord.js

--- src/data/discord.js
+++ src/data/discord.js
@@ -1,6 +1,6 @@
 const settings = {
-  discordID: "1073173936583479408",
-  showPresence: false, // Make it true if you want to show your Discord Presence.
+  discordID: "770227564442026024",
+  showPresence: true, // Make it true if you want to show your Discord Presence.
 };
 
 export default settings;
