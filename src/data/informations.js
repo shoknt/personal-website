a1eb3654eb04b4d6a205d622e714a42406034c2c
shoknt <153351426+shoknt@users.noreply.github.com>
2024-07-01 06:49:00 +0700

    Update informations.js

--- src/data/informations.js
+++ src/data/informations.js
@@ -1,17 +1,7 @@
 const me = {
   name: "shok",
   nickname: "shoknt",
-  bio: "Hey there, I'm chok!
-
-
-
-Started managing & moderating communities over a decade ago as a volunteer (forum moderator, game Game moderator, game helper, etc) in several forums & games with the goal of assisting users and building safe & healthy communities in the gaming sphere. That same goal has been my motivator to continuously grow, hone, and practice my skills as I worked in Customer Support, Content Moderation, and Community Management.
-
-
-
-Working directly with Discord on launching several VIP & brand communities as part of their Discord Moderator Program has led to so many amazing learning opportunities in regards to managing different types of communities and audiences.
-
-I started programming at a young age, and I am improving myself every day",
+  bio: "Hey there, I'm chok! Started managing & moderating communities over a decade ago as a volunteer (forum moderator, game Game moderator, game helper, etc) in several forums & games with the goal of assisting users and building safe & healthy communities in the gaming sphere. That same goal has been my motivator to continuously grow, hone, and practice my skills as I worked in Customer Support, Content Moderation, and Community Management.Working directly with Discord on launching several VIP & brand communities as part of their Discord Moderator Program has led to so many amazing learning opportunities in regards to managing different types of communities and audiences.",
   status: "bot discord developer, Full-time Community Manager, also full-time dog parent.",
   keywords: [
     "shoknt",
