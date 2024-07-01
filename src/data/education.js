75cd11a4220008ae203aeeb735a3fc4ed6f69caf
shoknt <153351426+shoknt@users.noreply.github.com>
2024-07-01 10:15:02 +0700

    Update education.js

--- src/data/education.js
+++ src/data/education.js
@@ -1,11 +1,11 @@
 const education = [
   {
-    date: "",
+    date: "-",
     title: "High School",
     value: "Unfortunately, I did not study but participated in the GED test.",
   },
   {
-    date: "",
+    date: "-",
     title: "University",
     value: "I studied at university about cyber security.",
   },
